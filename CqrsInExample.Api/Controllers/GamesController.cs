c9afad3fe1ec603305c2e67d9ffa7ad7e99c6b62
GitLa <GitLa@GITLA>
2023-06-19 11:26:38 +0300

    Add project files.

---
+++ CqrsInExample.Api/Controllers/GamesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsInExample.Api.Controllers
+{
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        public GamesController()
+        {
+                
+        }
+
+
+    }
+}
